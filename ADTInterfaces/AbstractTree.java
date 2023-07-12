bbde427aa82e0e91e445768edcb516ddaf75f01d
BlueBowser10 <pitabread441@gmail.com>
2023-07-12 10:27:03 -0400

    worked more on Tree documentation, turned TreeInterface into abstract clas AbstractTree

--- ADTInterfaces/AbstractTree.java
+++ ADTInterfaces/AbstractTree.java
@@ -1,30 +1,92 @@
 package ADTInterfaces;
+import java.security.InvalidAlgorithmParameterException;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
- * An interface describing the operations that can be done by the Tree ADT. It
- * will be implemented later by a class.
+ * An abstract class describing the operations that can be done by the Tree
+ * ADT. It will be implemented later by a class. This abstract base class
+ * describes common behavior and structure of all Trees.
+ * @param <T> the type that the tree holds
+ * @author BlueBowser.
  */
-public interface TreeInterface<T> extends Iterable<T> {
+public abstract class AbstractTree<T> implements Iterable<T> {
+    /**
+     * Internal node class which represents a node on the tree.
+     * @param <T> the type the node holds.
+     */
+    protected static class Node<T> implements Position<T> {
+        T element;
+        Node<T> parent;
+        ArrayList<Node<T>> childs;
+
+        /**
+         * Creates a new tree Node holding an element
+         * @param elem the element the Node will hold. Must not be null.
+         * @throws IllegalArgumentException if the element passed is null.
+         */
+        pubilc Node(T elem) throws IllegalArgumentException {
+            if (elem == null) {
+                throw new IllegalArgumentException("node cannot hold a null value!");
+            }
+            this.element = elem;
+            this.childs = new ArrayList<Node<T>>();
+        }
+
+        /**
+         * @return the element stored by the Node.
+         */
+        public T getElement() {}
+
+        /**
+         * Sets Node to hold a new element {@code elem}.
+         * @param elem the new element to store in the Node. Must not be null.
+         * @throws IllegalArgumentException if the elemenet passed is null.
+         */
+        public void setElement(T elem) throws IllegalArgumentException {}
+
+        /**
+         * @return the parent of the Node.
+         */
+        public Node<T> parent() {}
+
+        /**
+         * @param newParent the new parent to set to the Node.
+         */
+        public void setParent(Node<T> newParent) {}
+
+        /**
+         * @return an ArrayList of all the children of the current Node.
+         */
+        public ArrayList<Node<T>> children() {}
+        /**
+         * Adds a new node as a child of the current Node.
+         * @param node the child to be added to the Node. Must not be null.
+         * @throws IllegalArgumentException if the value is null.
+         */
+        public void addChild(Node<T> node) throws IllegalArgumentException {}
+        public Node<T> removeChild(Node<T> node) {}
+    }
     /**
      * The size of the tree.
      * @return the number of nodes in the tree
      */
-    public int size();
+    public abstract int size();
 
     /**
      * Checks if the tree is empty.
      * @return {@code true} if the tree has no Nodes, {@code false}
      * otherwise.
      */
-    public int isEmpty();
+    public abstract boolean isEmpty();
 
     /**
      * Returns the root's Position on the tree, returning null if the
      * tree is empty.
-     * @return a Position pointing to the root of the tree
+     * @return a Position pointing to the root of the tree, or {@code null}
+     * if the tree is empty.
      */
-    public Position<T> root();
+    public abstract Position<T> root();
 
     /**
      * Reuturns the position of the parent node of {@code p}, returning null
@@ -32,29 +94,29 @@ public interface TreeInterface<T> extends Iterable<T> {
      * @param p the Position of a node
      * @return the parent of a node
      */
-    public Position<T> parent(Position<T> p);
+    public abstract Position<T> parent(Position<T> p);
 
     /**
-     * Returns an iterable collection of all the children of a node
+     * Returns an iterable collection of all the children of a node.
      * @param p the Position of a node
      * @return an object implementing Iterable that also contains the children
      * of the node at a Posiiton {@code p}
      */
-    public Iterable<T> children(Position<T> p);
+    public abstract Iterable<T> children(Position<T> p);
 
     /**
      * Returns the number of children at a node.
      * @param p the Position of the node.
      * @return the number of childrena node has
      */
-    public int numChilds(Position<T> p);
+    public abstract int numChilds(Position<T> p);
 
     /**
      * Checks if a node is internal (that the node has at least one child).
      * @param p a Position of a node
      * @return {@code true} if the node is internal, {@code false} otherwise
      */
-    public boolean isInternal(Position<T> p);
+    public abstract boolean isInternal(Position<T> p);
 
     /**
      * Checks whether the node is a leaf/external (that the node has no 
@@ -62,21 +124,30 @@ public interface TreeInterface<T> extends Iterable<T> {
      * @param p a Position of a node
      * @return {@code true} if the node is a leaf, {@code false} otherwise
      */
-    public boolean isLeaf(Position<T> p);
+    public abstract boolean isLeaf(Position<T> p);
 
     /**
      * Checks whether the node is a root node (it has no parent)
      * @param p the Position of a node
      * @return {@code true} if the Node is a root, {@code false} otherwise
      */
-    public boolean isRoot(Position<T> p);
+    public abstract boolean isRoot(Position<T> p);
 
+    /**
+     * Iterates through all the elements in a tree in a breadth first traversal.
+     * @return an Iterator going through the tree in a breadth first traversal.
+     */
     public Iterator<T> iterator();
+    /**
+     * Iterates through all the Positions of the tree ina breadth first
+     * traversal.
+     * @return an Iterator going through the tree in a breadth first traversal.
+     */
     public Iterator<Position<T>> positions();
 
     /**
      * Returns the depth of a Node. The depth is how far the Node is from the
-     * root node (how many ancestors does the node have?). The depth of the 
+     * root node (how many ancestors does the node have). The depth of the 
      * root node is 0, and increases the more parents you have to get through
      * to reach the root node
      * @param p the position of the Node
@@ -142,15 +213,6 @@ public interface TreeInterface<T> extends Iterable<T> {
      * @throws IllegalStateException if the tree is not empty
      */
     public Position<T> addRoot(T elem) throws IllegalStateException;
-
-    /**
-     * Adds a child to the node at Position {@code parent}
-     * containing the element {@code elem}.
-     * @param parent the Position of the node to add a child to
-     * @param elem
-     * @return the Position of the new child node.
-     */
-    public Position<T> addChild(Position<T> parent, T elem);
     /**
      * Sets the element located at Position {@code p} to {@code elem}
      * @param p Position of a certain node
@@ -158,23 +220,6 @@ public interface TreeInterface<T> extends Iterable<T> {
      * @return the last element stored at that Posiiton
      */
     public T set(Position<T> p, T elem);
-
-    /**
-     * Attatches a tree as a child to to Position {@code p}, making the root of
-     * the {@code tree} a chlild of Position {@code p}
-     * @param p the Position of a node. Node must be a leaf node.
-     * @param tree the tree to attach to the branch
-     * @throws IllegalStateException if {@code p} is not a leaf
-     */
-    public void attach(Position<T> p, TreeInterface<T> tree) throws IllegalStateException;
-    /**
-     * Removes node Position {@code p} from the tree, and make all of its
-     * children the children of the parent node of Position {@code p}
-     * @param p
-     * @return the element of the deleted Node.
-     */
-    public T hoist(Position<T> p);
-
     /**
      * Discards a node Position {@code p} from the tree, including all its
      * descendants.
---
+++ ADTInterfaces/GeneralTreeInterface.java
@@ -0,0 +1,35 @@
+package ADTInterfaces;
+
+/**
+ * This imterface describes the behavior of a very general Tree with no
+ * restrictions on the number of children a node can have. This will be
+ * implemented later by a class.
+ * @param <T> the type the tree holds
+ * @author BlueBowser
+ * @see AbstractTree for more info on the other methods.
+ */
+public class LimitedTree<T> extends AbstractTree<T> {
+    /**
+     * Adds a child to the node at Position {@code parent}
+     * containing the element {@code elem}.
+     * @param parent the Position of the node to add a child to
+     * @param elem
+     * @return the Position of the new child node.
+     */
+    public Position<T> addChild(Position<T> parent, T elem);
+    /**
+     * Attatches a tree as a child to to Position {@code p}, making the root of
+     * the {@code tree} a chlild of Position {@code p}
+     * @param p the Position of a node. Node must be a leaf node.
+     * @param tree the tree to attach to the branch
+     * @throws IllegalStateException if {@code p} is not a leaf
+     */
+    public void attach(Position<T> p, AbstractTree<T> tree) throws IllegalStateException;
+    /**
+     * Removes node Position {@code p} from the tree, and make all of its
+     * children the children of the parent node of Position {@code p}
+     * @param p the Position of the node to be removed.
+     * @return the element of the deleted Node.
+     */
+    public T hoist(Position<T> p);
+}
